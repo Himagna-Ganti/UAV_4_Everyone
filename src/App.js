e32ef9653faaecea9a356c8ac3a216400cf07c68
Himagna Ganti <himagna.ganti6@gmail.com>
2023-09-27 09:35:28 -0400

    wadn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
-
+import Intro from "./components/Intro";
 function App() {
   return (
     <>
       <Navbar />
       <br />
       <Title />
+      <Intro />
     </>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="  h-[200px]   mr-[150px] ml-[150px] font-work text-2xl text-justify">
+      <span className="text-7xl">U</span>nmanned Aerial Vehicles (UAVs) have
+      become crucial for surveillance and infrastructure inspection, but their
+      effective use relies heavily on data-intensive machine learning models for
+      tasks such as object detection and collision avoidance. However, the high
+      cost of acquiring and labeling data, coupled with limited data
+      availability, hinders progress in this field. To address this challenge, a
+      collaborative portal is proposed, aiming to facilitate the sharing of UAV
+      datasets and project code among researchers and the industry. This
+      approach, inspired by successful models in medical and computer vision
+      domains, seeks to accelerate advancements in UAV-related applications.
+      Canadian departments and agencies have recognized the importance of
+      counter-drone research to prevent unauthorized drone activity, while
+      infrastructure inspection using UAVs is vital for security. By fostering
+      innovation and cooperation, this collaborative portal intends to propel
+      the growth of UAV-related technologies and their applications.
+    </div>
+  );
+}
+
+export default Intro;
