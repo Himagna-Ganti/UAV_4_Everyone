8bdbb5b5a466ca8c6c5e058b8e7c1e0e5d7cc7b6
AbhinayReddy24 <abhinayreddy2409@gmail.com>
2023-10-03 22:24:52 -0400

    added clickable text to show images when clicked

--- src/components/Datasets.js
+++ src/components/Datasets.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Table from "./Table"; // Import the Table component
 import Navbar from "./Navbar";
+// import Datasetpreview from "./Datasetpreview";
+import Rectangularcomponent from "./Rectangularcomponent";
 
 function Datasets() {
   const data = [
@@ -8,7 +10,11 @@ function Datasets() {
     { id: 2, name: "Dataset 2", description: "Description 2" },
     // Add more dataset objects as needed
   ];
-
+  // const imagePaths = [
+  //   './Images/Abhi.png',
+  //   './Images/Himagna.png'
+  //   // Add more image paths here
+  // ];
   return (
     <div>
       <Navbar /> {/* Include the Navbar component */}
@@ -18,6 +24,11 @@ function Datasets() {
         <br />
       <h1>Datasets</h1>
       <Table data={data} /> {/* Include the Table component with dataset data */}
+      {/* <Datasetpreview imagePaths={imagePaths} /> */}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "50vh" }}></div>
+      {/* <dic className="rectangular-component"/> */}
+      <div className="center-container"></div>
+      <Rectangularcomponent />
     </div>
   );
 }
