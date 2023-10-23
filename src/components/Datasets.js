0bfa817ec13dc125b8562ce723d9e73bf3b972a7
AbhinayReddy24 <abhinayreddy2409@gmail.com>
2023-10-22 21:03:30 -0400

    Revert "UAV-48: Added link to dirfts component to open datasets"
    This reverts commit 5f35ee3ed94930641763664e05169aca4c429a0d.

--- src/components/Datasets.js
+++ src/components/Datasets.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Table from "./Table";
+import "./Table.css";
 import Navbar from "./Navbar";
 // import Infobox from './Infobox';
 import Multiplelinebreaks from "./Mulitplelinebreaks";
+import Modal from "./Modal"; // Import the Modal component
 import "./Modal.css"; 
-import Driftbox from "./Driftbox";
-
 
 function Datasets() {
+  const data = [
+    { id: 1, name: "Dataset 1", description: "Description 1" },
+    { id: 2, name: "Dataset 2", description: "Description 2" },
+    // Add more dataset objects as needed
+  ];
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+
+  const openModal = (data) => {
+    setSelectedData(data);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedData(null);
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
       <Navbar /> 
       <Multiplelinebreaks count={4} />
       <h1>Datasets</h1>
-      <Driftbox/>
-      <Multiplelinebreaks count={4} />
+      <Table data={data} onNameClick={openModal} /> {/* Pass the openModal function */}
+      <Multiplelinebreaks count={3} />
+      {/* <Infobox
+        title="Dataset 1"
+        content="Dataset 1 details"
+      /> */}
+      <Modal isOpen={isModalOpen} data={selectedData} onClose={closeModal} /> {/* Render the Modal */}
     </div>
-    
   );
 }
 
