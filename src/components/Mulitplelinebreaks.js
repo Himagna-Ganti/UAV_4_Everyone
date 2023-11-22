e0fc1e6eaa04fa9fb252101dfd00f857cfb45b5c
AbhinayReddy24 <abhinayreddy2409@gmail.com>
2023-11-21 22:07:49 -0500

    UAV-69,70,77:added components to know more and about page

--- src/components/Mulitplelinebreaks.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Multiplelinebreaks({ count }) {
-  const lineBreaks = Array(count).fill(<br />);
-
-  return <>{lineBreaks}</>;
-}
-
-export default Multiplelinebreaks;
